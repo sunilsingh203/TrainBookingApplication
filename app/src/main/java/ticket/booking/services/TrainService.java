4662cdb824a4dda9e25e444f1a7425cdf523740c
sunilsingh203 <sunilsingh81188@gmail.com>
2025-03-18 21:53:12 +0530

    added local databases for train and users

--- app/src/main/java/ticket/booking/services/TrainService.java
+++ app/src/main/java/ticket/booking/services/TrainService.java
@@ -1,4 +1,5 @@
 package ticket.booking.services;
 
 public class TrainService {
+
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,4 +1,10 @@
 package ticket.booking.services;
 
+import ticket.booking.entities.User;
+
 public class UserBookingService {
+    private User user;
+    public UserBookingService(User user1) {
+        this.user = user1;
+    }
 }
