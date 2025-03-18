55fdc9f22e4905ccbfd27a6f6b66a1edc4214434
sunilsingh203 <sunilsingh81188@gmail.com>
2025-03-18 22:07:41 +0530

    added required dependencies in bulid.gradle file

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,9 +2,13 @@ package ticket.booking.services;
 
 import ticket.booking.entities.User;
 
+import java.io.File;
+
 public class UserBookingService {
     private User user;
+    private static final String  USERS_PATH = "../localDb/users.json";
     public UserBookingService(User user1) {
         this.user = user1;
+        File users = new File(USERS_PATH);
     }
 }
